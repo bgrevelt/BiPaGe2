d5c7d111bf85817f25830d00ec0692a92d856fb7
Bouke <b.grevelt@gmail.com>
2021-02-26 23:31:14 +0100

    #57 Added collection class. Very simple class that wraps a pointer and a size and an std::array-like interface.

---
+++ library/c++/BiPaGe/Collection.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <type_traits>
+
+namespace BiPaGe
+{
+    template<typename T, size_t COLLECTION_SIZE, typename Enable=void>
+    class Collection;
+
+    template<typename T, size_t COLLECTION_SIZE>
+    class Collection<T,COLLECTION_SIZE, typename std::enable_if<std::is_pod<T>::value, void>::type>
+    {
+    public:
+        Collection(const void* data)
+            : data_(reinterpret_cast<const T*>(data))
+        {
+
+        }
+
+        const T* begin() const
+        {
+            return data_;
+        }
+        const T* end() const
+        {
+            return begin() + COLLECTION_SIZE;
+        }
+        const T& at(size_t index) const
+        {
+            assert(index < COLLECTION_SIZE);
+            return *(begin() + index);
+        }
+        const T& back() const
+        {
+            return *(begin() + COLLECTION_SIZE -1);
+        }
+        const T& front() const
+        {
+            return *begin();
+        }
+        size_t size() const
+        {
+            return COLLECTION_SIZE;
+        }
+        const T& operator[](size_t index) const
+        {
+            return at(index);
+        }
+    private:
+        const T* const data_;
+    };
+}
